ba0e8ed997629c4ee7bba5b2ea4de0f87b26c5e4
Joy Evans <joyevans@me.com>
2021-10-25 11:55:18 -0500

    added index.js

---
+++ Sources/src/index.js
@@ -0,0 +1,35 @@
+console.log("hello")
+
+let element = document.getElementsByName('main');
+let btn = document.getElementById('btn-transition')
+
+// function smoothScroll() {
+  btn.addEventListener('click', function() {
+    // transition to the 'main' content
+    alert("got it")
+    // element.scrollIntoView(alignToTop)
+  })
+// }
+
+function validateForm() {
+    let x = document.forms["myForm"]["task"].value;
+    if (x == "") {
+      alert("Don't forget to add a task.");
+      return false;
+    }
+    else {
+        alert("hooray")
+        return true;
+    }
+  }
+  //Create the element using the createElement method.
+let myDiv = document.createElement("div");
+
+//Set its unique ID.
+myDiv.id = 'div_id';
+
+//Add your content to the DIV
+myDiv.innerHTML = x;
+
+//Finally, append the element to the HTML body
+document.body.appendChild(myDiv);
